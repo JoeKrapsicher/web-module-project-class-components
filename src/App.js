c7edaff748424a63d0d991dce055131187d04ece
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-07-12 15:43:48 -0600

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,96 @@
 import React from 'react';
+import "./components/Todo.css";
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
-class App extends React.Component {
+
+// The default data, its gonna be the heart of our application
+
+const allTasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+class App extends React.Component { // Right here we are extending the the base component 'Component'
+
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      allTasks: allTasks
+    }
+  }
+
+  // toggleTask is meant to look over our data and any item that need to be toggled completed or not
+
+  toggleTask = taskId => {
+    console.log("Toggling task", taskId)
+    //map over the todolist array
+    //when we find the one clicked, toggle its completed flag
+    const updatedTasks = this.state.allTasks.map(item => {
+      if (taskId === item.id) {
+        return {...item, completed: !item.completed}
+      }
+      return item;
+    });
+
+    console.log("updated groceries array", updatedTasks)
+
+    // now that we created all the updated tasks we have to merge that data back into state
+
+    this.setState({
+      ...this.state,
+      allTasks: updatedTasks
+    });
+  }
+
+  //this function is gonna be prop drilled into TodoForm, the point of this function is to create a new item from list and have it's default 
+  //properties be given with names of, taskName: users input, id: Date.now() time id, and default completed to false
+
+  handleAddEvent = taskName => {
+    this.setState({
+      ...this.state,
+      allTasks: [...this.state.allTasks, 
+      {
+        task: taskName,
+        id: Date.now(),
+        completed: false
+      }]
+    })
+  }
+
+  // this function's purpose is to clear already completed tasks and removing them from state. 
+
+  clearCompleted = e =>{
+    console.log("Completed", allTasks)
+    //filter all items completed: true
+    e.preventDefault();
+    this.setState({
+      ...this.state,
+      
+      // deconstructing: allTasks become allTasks without items that arent completed
+
+      allTasks: this.state.allTasks.filter(item => !item.completed)
+    })
+  }
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>Welcome to your Todo App!</h1> 
+          <TodoForm handleAddEvent={this.handleAddEvent}/>
+        </div>
+          <TodoList clearCompleted={this.clearCompleted} toggleTask={this.toggleTask} allTasks={this.state.allTasks}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,27 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+import Todo from './Todo'
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+//<TodoList />` receives your Todos array and iterates over the list generating a new `<Todo />` for each element in the array.
+
+const TodoList = props => {
+    // for sorting the list based on whether an item has been purchased or not
+    // const sortedList = props.allTasks.sort((a, b) => a.purchased - b.purchased);
+    return(
+        <div className="task-list">
+            <div>
+                {props.allTasks.map(item => { return(
+                    <Todo toggleTask={props.toggleTask} key={item.id} item={item}/>
+                )})}
+            </div>
+            <button onClick={props.clearCompleted} className="clear-btn">
+                Clear Completed
+            </button>
+        </div>
+    )
+}
+
+export default TodoList
